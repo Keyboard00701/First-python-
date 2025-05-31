1878111d84458b7243db92ffeea90e146a408a80
... <death9ix00701@gmail.com>
2025-05-30 20:33:35 -0700

    my first print
    you need to make a comment to push lol super annoying! this can help devs see what and why the code was pushed and what envs where made!!!

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,16 @@
 print ("hello world")
+
+# this is a comment, this is how you write a comment in python, it wont affect the code but helps devs
+# talk in a code script and the computer will ignore it but will help the devs communicate!!!
+
+# let's try to print a nother terminal command!
+
+print ("keep it like this")
+
+print ("im a goat")
+
+# make sure when printing comments or writing none text it happens like this or it will break the code
+# you will get somehitng called syntax error or bugs let me show you
+
+print (" im a goat")
+print ("killing my brain with basic lol")
